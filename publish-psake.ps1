b3bd165e35d91a27ecf30264bb1709d476e39cf5
Wolfgang Gross <github@text-mode.de>
2022-04-30 12:20:51 +0200

    [REFACTOR] Dependencies updated

--- publish-psake.ps1
+++ publish-psake.ps1
@@ -1,10 +1,11 @@
 Import-Module fs-dirs
 Import-Module Microsoft.PowerShell.SecretManagement
 
-Task secret {
+Task secret-loaded {
    
     $script:secret = Get-Secret -Name powershellGallery -AsPlainText
-} 
+    
+} -precondition ($null -ne $script:secret)
 
 Task clean-compile {
 
@@ -14,17 +15,20 @@ Task clean-compile {
     }
 }
 
+Task publish-filesystem {
+    # create a file system structire for publishing to the gallery
+    New-Item  -Path "FSWatcherEngineEvent\1.2" -ItemType Directory -ErrorAction SilentlyContinue
+    Get-ChildItem -File | Copy-Item -Destination "FSWatcherEngineEvent\1.2"
+} -depends clean-compile
+
 Task publish-gallery {
 
     "$PSScriptRoot\src\FSWatcherEngineEvent\bin\Debug\netstandard2.0\publish" | fs-dirs\Invoke-AtContainer {
-        # create a file system structire for publishing to the gallery
-        New-Item  -Path "FSWatcherEngineEvent\1.2" -ItemType Directory -ErrorAction SilentlyContinue
-        Get-ChildItem -File | Copy-Item -Destination "FSWatcherEngineEvent\1.2"
-
+    
         # now publish to the gallery
         Publish-Module -Path "FSWatcherEngineEvent\1.2" -NuGetApiKey $script:secret -Verbose 
     }
 
-} -depends secret,clean-compile
+} -depends secret-loaded,publish-filesystem
 
 Task default -depends publish-gallery
