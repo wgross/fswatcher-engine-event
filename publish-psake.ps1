ce11e39806c68c1a8ce79c8ff79aac71dabbabb4
Wolfgang Gross <github@text-mode.de>
2022-05-23 13:23:31 +0200

    [RELEASE] published v1.4 to gallery
    - readme adapted
    - version bumped

--- publish-psake.ps1
+++ publish-psake.ps1
@@ -1,15 +1,23 @@
 Import-Module fs-dirs
 Import-Module Microsoft.PowerShell.SecretManagement
 
+properties {
+    $rootDir = $PSScriptRoot
+    $publishDirectory = "$PSScriptRoot\FSWatcherEngineEvent\1.4"
+}
+
 Task secret-loaded {
    
     $script:secret = Get-Secret -Name powershellGallery -AsPlainText
+    $script:secret|Write-Debug
     
 } -precondition { $null -eq $script:secret }
 
 Task clean-compile {
 
-    "$PSScriptRoot\src\FSWatcherEngineEvent"  | fs-dirs\Invoke-AtContainer {
+    Write-Host "Cleaning project: $publishDirectory"
+
+    "$rootDir\src\FSWatcherEngineEvent"  | fs-dirs\Invoke-AtContainer {
         dotnet clean 
         dotnet publish 
     }
@@ -17,18 +25,20 @@ Task clean-compile {
 
 Task publish-filesystem {
     
-    # create a file system structure for publishing to the gallery
-    $script:publishDirectory = New-Item  -Path "$PSScriptRoot\FSWatcherEngineEvent\1.3" -ItemType Directory -ErrorAction SilentlyContinue
+    Write-Host "Publish directory: $publishDirectory"
+    Test-Path $publishDirectory
+    
+    New-Item  -Path "$PSScriptRoot\FSWatcherEngineEvent\1.4" -ItemType Directory -ErrorAction SilentlyContinue
 
-    "$PSScriptRoot\src\FSWatcherEngineEvent\bin\debug\netstandard2.0\publish"  | fs-dirs\Invoke-AtContainer {
-        Get-ChildItem -File | Copy-Item -Destination $script:publishDirectory -Exclude @("run.ps1","init.ps1") -Force
+    "$rootDir\src\FSWatcherEngineEvent\bin\debug\netstandard2.0\publish"  | fs-dirs\Invoke-AtContainer {
+        Get-ChildItem -File | Copy-Item -Destination $publishDirectory -Exclude @("run.ps1","init.ps1") -Force
     }
 
 } -depends clean-compile
 
 Task publish-gallery {
 
-    $script:publishDirectory | fs-dirs\Invoke-AtContainer {
+    $publishDirectory | fs-dirs\Invoke-AtContainer {
     
         # now publish to the gallery
         Publish-Module -Name .\FSWatcherEngineEvent.psd1 -NuGetApiKey $script:secret -Verbose 
--- src/FSWatcherEngineEvent/FSWatcherEngineEvent.psd1
+++ src/FSWatcherEngineEvent/FSWatcherEngineEvent.psd1
@@ -1,6 +1,6 @@
 @{
     RootModule = 'FSWatcherEngineEvent.dll'
-    ModuleVersion = '1.3'
+    ModuleVersion = '1.4'
     GUID = 'cea9a314-3e4c-4080-9a0b-5a867efa61f3'
     Author = 'github.com/wgross'
     Copyright = '(c) github.com/wgross. All rights reserved.'
@@ -22,6 +22,7 @@
             1.1: compatibility with netstandard2.0 (PowerShell 5.x)
             1.2: shortcuts the (un-)register event handlers when creating and removing file system watchers.
             1.3: remove file system listener when removing the module from the session
+            1.4: presents small text UI for editing watcher parameters before create
 "@
         }
     }
