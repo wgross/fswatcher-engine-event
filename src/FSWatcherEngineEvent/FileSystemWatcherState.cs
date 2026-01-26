0116201e8ac162a20b682f02871f65e9d964d055
Wolfgang Groß <github@text-mode.de>
2026-01-26 17:36:29 +0100

    - state is record now (Net 10.0)

--- src/FSWatcherEngineEvent/FileSystemWatcherState.cs
+++ src/FSWatcherEngineEvent/FileSystemWatcherState.cs
@@ -2,29 +2,11 @@
 
 namespace FSWatcherEngineEvent;
 
-public class FileSystemWatcherState
-{
-    public FileSystemWatcherState(
-        string sourceIdentifier,
-        string path,
-        NotifyFilters notifyFilters,
-        bool enableRaisingEvents,
-        bool includeSubdirectories,
-        string filter
-    )
-    {
-        this.SourceIdentifier = sourceIdentifier;
-        this.Path = path;
-        this.NotifyFilter = notifyFilters;
-        this.EnableRaisingEvents = enableRaisingEvents;
-        this.IncludeSubdirectories = includeSubdirectories;
-        this.Filter = filter;
-    }
-
-    public string SourceIdentifier { get; }
-    public string Path { get; }
-    public NotifyFilters NotifyFilter { get; }
-    public bool EnableRaisingEvents { get; }
-    public bool IncludeSubdirectories { get; }
-    public string Filter { get; }
-}
+public record FileSystemWatcherState(
+    string SourceIdentifier,
+    string Path,
+    NotifyFilters NotifyFilter,
+    bool EnableRaisingEvents,
+    bool IncludeSubdirectories,
+    string[] Filter
+);
